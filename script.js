008a4d875002c703527fa756c520d59f66222516
Uday Gupta <153746090+Guptaaudy@users.noreply.github.com>
2024-06-29 20:22:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function isSameType(value1, value2) {
-  //your js code here
+  if (value1===value2){
+	  alert("true")
+  }
+  else if(value1!==value2) {
+	  alert("false")
+  }
 }
+
 
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
